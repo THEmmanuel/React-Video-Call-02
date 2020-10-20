883afc69d576127465111ff2984b745af8845d79
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-10-20 12:29:02 +0100

    Added App.js function body

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,78 @@ import MainWindow from './containers/MainWindow';
 import './styles/App.css'
 
 const App = () => {
+    const [ClientToken, setClientToken] = useState('');
+    const [CallWindow, setCallWindow] = useState('')
+    const [CallModal, setCallModal] = useState('')
+    const [CallFrom, setCallFrom] = useState('')
+    const [localSrc, setLocalSrc] = useState(null)
+    const [peerSrc, setPeerSrc] = useState(null)
+
+    let peerConnection = {};
+    let Config = null;
+
+    useEffect(() => {
+        socket
+            .on('init', ({ token: ClientToken }) => {
+                document.title = `${ClientToken} - Video Call`;
+                setClientToken({ ClientToken })
+            })
+            .on('request', ({ from: CallFrom }) => {
+                setCallModal({ CallModal: 'active', CallFrom })
+            })
+            .on('call', data => {
+                if (data.SDP) {
+                    peerConnection.setRemoteDescription(data.SDP);
+                    if (data.SDP.type === 'offer') {
+                        peerConnection.createAnswer();
+                    }
+                }
+                else {
+                    peerConnection.addIceCandidate(data.candidate);
+                }
+            })
+
+            .on('end', endCallHandler.bind(this, false))
+            .emit('init')
+    }, [])
+
+
+    const startCallHandler = (isCaller, friendToken, config) => {
+        config = Config;
+        peerConnection = new PeerConnection(friendToken)
+            .on('localStream', src => {
+                if (!isCaller) {
+                    setCallModal('')
+                }
+            })
+            .on('peerStream', src => setPeerSrc(src))
+            .start(isCaller, config);
+    }
+
+    const rejectCallHandler = () => {
+        socket.emit('end', { to: CallFrom });
+        setCallModal('')
+    }
+
+    const endCallHandler = isStarter => {
+        if (_.isFunction(peerConnection.stop)) {
+            peerConnection.stop(isStarter);
+        }
+
+        peerConnection = {};
+        Config = null;
+        setCallWindow('');
+        setCallModal('');
+        setLocalSrc(null);
+        setPeerSrc(null);
+    }
+
+
     return (
-        <MainWindow/>
+        <MainWindow />
     )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
+export default App;
