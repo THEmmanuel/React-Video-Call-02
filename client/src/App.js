0a50d69cd93070b853bf1e687422b18630aa6438
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-10-20 09:17:22 +0100

    Added MainWindow imports to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import MainWindow from './containers/MainWindow';
 
 const App = () => {
     return (
-        <span>
-            Hello!
-        </span>
+        <MainWindow/>
     )
 }
 
