c7789c99b7ca6d01591927f992a838118a75ba5a
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-10-23 08:02:36 +0100

    Added style imports to CallWindow.js

--- client/src/containers/CallWindow.js
+++ client/src/containers/CallWindow.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import style from '../styles/CallWindow.module.css';
 
 const CallWindow = () => {
 
