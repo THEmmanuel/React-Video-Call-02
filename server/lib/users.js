5cc135e3901f14c79de93318521fd2e37d66c143
Emmanuel Ayodele <emmayodayo@gmail.com>
2020-11-29 20:26:43 +0100

    Changed token length to 7 characters

--- server/lib/users.js
+++ server/lib/users.js
@@ -3,10 +3,10 @@ const generateToken = require('./tokenGenerator');
 const users = {}
 
 async function uniqueToken(){
-    let token = generateToken(12);
+    let token = generateToken(7);
     while (token in users){
         await Promise.delay(5);
-        token = generateToken(12);
+        token = generateToken(6);
     }
     return token;
 }
